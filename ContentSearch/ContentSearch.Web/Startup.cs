b0c7a6b99de746d78b2e155ecd0753ebf1f5a071
Vishal Dedania <vdedaniya@gmail.com>
2018-06-29 23:22:36 -0400

    Setting up page layout

--- ContentSearch/ContentSearch.Web/Startup.cs
+++ ContentSearch/ContentSearch.Web/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ContentSearch.Web
@@ -15,6 +10,8 @@ namespace ContentSearch.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddControllersAsServices();
+            services.AddResponseCaching();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,12 +20,12 @@ namespace ContentSearch.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            app.UseResponseCaching();
         }
     }
 }
